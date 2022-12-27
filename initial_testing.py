c54ab4a63eec0b9ba9bad98c30b6a813963ca897
Markus <markus@linux.fritz.box>
2022-12-27 21:59:24 +0100

    added home page

--- initial_testing.py
+++ initial_testing.py
@@ -5,6 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/pubchem/<pubchem_id>")
 def pubchem(pubchem_id):
     return render_template("pubchem.html", pubchem_id=pubchem_id)
